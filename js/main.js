42c5c04c1c046f4ca5a5c49ab83853a9521aca57
Your Name <you@example.com>
2022-06-02 21:45:25 +0300

    contact + fixed transition

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 //Welcome
 const welcomeBtn = document.querySelector('.welcome__btn')
 const header = document.querySelector('.header')
-window.addEventListener('scroll',function (){
-    console.log(window.scrollY)
-
-})
 welcomeBtn.addEventListener('click',function (e) {
     e.preventDefault()
     document.querySelector('.welcome').classList.remove('active')
@@ -27,10 +23,8 @@ window.addEventListener('scroll',function () {
 //cube animation
 const cubeBtn = document.querySelector('.cube')
 const contentLeft = document.querySelector('.bio__left')
-const contentRight = document.querySelector('.bio__right')
 
 cubeBtn.addEventListener('click',function () {
     contentLeft.classList.add('active')
-    contentRight.classList.add('active')
     cubeBtn.classList.toggle('active')
 })
