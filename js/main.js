1d7a778464e2c3b22331f3371ad6841a98e4966b
Your Name <you@example.com>
2022-06-04 01:40:43 +0300

    photo mouse anim

--- js/main.js
+++ js/main.js
@@ -21,6 +21,33 @@ window.addEventListener('scroll',function () {
         header.classList.remove('active')
     }
 })
+
+// bio photo
+
+const photo = document.querySelector('.bio__right')
+const photoInner = document.querySelector('.bio__photo')
+
+
+// photo.addEventListener('mousemove',function (e){
+//     cx = window.innerWidth / 2
+//     cy = window.innerHeight / 2
+//     photoW = photo.offsetWidth
+//     photoH = photo.offsetHeight
+//     centerX = photoW / 2
+//     centerY = photoH / 2
+//     // console.log(centerX)
+//     clientX = e.pageX
+//     clientY = e.pageY
+//
+//     tiltX = (cx + centerX + 85) - clientX
+//     tiltY = cy - clientY
+//     console.log(tiltX, tiltY)
+//     photoInner.style.transform = 'translate(' + -tiltX / 12 + 'px,' + -tiltY / 12 + 'px) scale(1.1)'
+//     if (tiltX >= 270) {
+//          photoInner.style.transform = 'translate(' + 0 + 'px,' + -tiltY / 12 + 'px) scale(1.1)'
+//     }
+// })
+
 //cube animation
 const cubeBtn = document.querySelector('.cube')
 const contentLeft = document.querySelector('.bio__left')
