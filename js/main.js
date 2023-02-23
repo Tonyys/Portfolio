14bf2d571d493b23273cf89e3d000ee7c3e06f79
Anton <tosha.makarchuk2@gmail.com>
2023-02-23 12:54:42 +0200

    upload commit

--- js/main.js
+++ js/main.js
@@ -12,40 +12,28 @@ welcomeBtn.addEventListener('click',function (e) {
     }
 })
 
-//Header fixed
-
-window.addEventListener('scroll',function () {
-    if (window.scrollY > 1) {
-        header.classList.add('active')
-    } else {
-        header.classList.remove('active')
-    }
-})
 
 // bio photo
 
-const photo = document.querySelector('.bio__right')
-const photoInner = document.querySelector('.bio__photo')
-
-
-// photo.addEventListener('mousemove',function (e){
+// const photo = document.querySelector('.bio__right')
+// const photoInner = document.querySelector('.bio__photo')
+//
+//
+// window.addEventListener('mousemove',function (e){
 //     cx = window.innerWidth / 2
 //     cy = window.innerHeight / 2
 //     photoW = photo.offsetWidth
 //     photoH = photo.offsetHeight
 //     centerX = photoW / 2
 //     centerY = photoH / 2
-//     // console.log(centerX)
 //     clientX = e.pageX
 //     clientY = e.pageY
 //
 //     tiltX = (cx + centerX + 85) - clientX
 //     tiltY = cy - clientY
 //     console.log(tiltX, tiltY)
-//     photoInner.style.transform = 'translate(' + -tiltX / 12 + 'px,' + -tiltY / 12 + 'px) scale(1.1)'
-//     if (tiltX >= 270) {
-//          photoInner.style.transform = 'translate(' + 0 + 'px,' + -tiltY / 12 + 'px) scale(1.1)'
-//     }
+//     photoInner.style.transform = 'rotateY(' + -tiltX / 100 + 'deg) rotateX(' + tiltY / 100 + 'deg)'
+//
 // })
 
 //cube animation
@@ -57,8 +45,31 @@ cubeBtn.addEventListener('click',function () {
     cubeBtn.classList.toggle('active')
 })
 
+//Header fixed
+
+window.addEventListener('scroll',function () {
+    if (window.scrollY > 1) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+})
+
+// header burger
+
+const burger = document.querySelector('.burger')
+const burgerContent = document.querySelector('.mobile')
+
+burger.addEventListener('click',function(){
+    burger.classList.toggle('active')
+    burgerContent.classList.toggle('active')
+    document.querySelector('body').classList.toggle('overflow')
+})
+
+
 // header link active
 const headerLinks = document.querySelectorAll('.header__link')
+const mobileLinks = document.querySelectorAll('.mobile__link')
 const windowHeight = window.innerHeight * 0.50
 
 window.addEventListener('scroll',function (){
@@ -84,11 +95,102 @@ headerLinks.forEach(function (item){
         scrollTo(currentSection)
     })
 })
+mobileLinks.forEach(function (item){
+    let currentItem = item
+    let itemAttr = currentItem.getAttribute('data-scroll')
+    let currentSection = document.querySelector(itemAttr)
+    item.addEventListener('click',function (e){
+        e.preventDefault()
+        scrollTo(currentSection)
+        burgerContent.classList.remove('active')
+        burger.classList.remove('active')
+        document.querySelector('body').classList.remove('overflow')
+    })
+})
 function scrollTo(element) {
     window.scroll({
         left: 0,
-        top: element.offsetTop,
+        top: element.offsetTop - 10,
         behavior: 'smooth'
     })
 }
+// Validate
+const inpName = document.querySelector('.form__name input')
+const inpEmail = document.querySelector('.form__email input')
+const inpMess = document.querySelector('.form__mess textarea')
+const form = document.querySelector('form')
+
+const regEmail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+let count = 0
+
+document.querySelector('.form__btn').addEventListener('click',function (e){
+    e.preventDefault()
+    if (inpName.value.length < 2) {
+        count--
+        inpName.classList.add('active')
+    } else {
+        count++
+        inpName.classList.remove('active')
+    }
+    if(!isEmail (regEmail, inpEmail.value) || inpEmail.value.length <= 3) {
+        count--
+        inpEmail.classList.add('active')
+    } else {
+        count++
+        inpEmail.classList.remove('active')
+    }
+    if (inpMess.value.length < 4){
+        count--
+        document.querySelector('.form__error').classList.add('active')
+    } else {
+        count++
+        document.querySelector('.form__error').classList.remove('active')
+    }
+    if (count < 3) {
+        count = 0
+    }
+    if (count === 3){
+        sendMail()
+        count = 0
+        document.querySelector('.popup').classList.add('active')
+        form.reset()
+    }
+})
+
+async function sendMail(){
+    let formData = new FormData(form)
+    let response = await fetch('mail.php',{
+        method: 'POST',
+        body: formData
+    })
+}
+document.querySelector('.popup__cross').addEventListener('click',function (){
+    document.querySelector('.popup').classList.remove('active')
+})
+document.querySelector('.popup__exit').addEventListener('click',function (){
+    document.querySelector('.popup').classList.remove('active')
+})
+function isEmail (regex, inp) {
+    return regex.test(inp)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- mail.php
+++ mail.php
@@ -1,41 +1,50 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+//change this to your email.
+$to = "toha.web.developer@gmail.com";
+$from = "Portfolio";
+$subject = "Робота(Портфоліо)";
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+//$name = 'name';
+//$social = 'social';
+//$socialType = 'socialtype';
+
 
 $name = $_POST['user_name'];
-$phone = $_POST['user_message'];
-$email = $_POST['user_email'];
+$social = $_POST['user_email'];
+$socialType = $_POST['user_message'];
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+//begin of HTML message
+$message ='
+               <html>
+                  <head>
+                      <title>Welcome</title>
+                  </head>
+                  <body>
+                      <h1>Повідомлення з <a href="http://kuran.ct8.pl/makarchuk/">Портфоліо</a></h1>
+                      <table cellspacing="0" cellpadding="10" style="border: 2px dashed silver; width: 40%;">
+                          <tr>
+                              <th>Ім`я:</th><td>'. $name .'</td>
+                          </tr>
+                          <tr>
+                              <th>Емайл:</th><td>'. $social .'</td>
+                          </tr>
+                          <tr>
+                              <th>Повідомлення:</th><td>'. $socialType .'</td>
+                          </tr>
+                      </table>
+                  </body>
+                  </html>';
+//end of message
+//$headers  = "From: $from\r\n";
+$headers = "Content-type: text/html\r\n";
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'tosha.makarchuk@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = '+_cvc123'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('tosha.makarchuk@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('tosha.makarchuk2@gmail.com
-');     // Кому будет уходить письмо
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name . ' оставил заявку, его сообщение ' .$phone. '<br>Почта этого пользователя: ' .$email;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
+if (mail($to, $subject, $message, $headers)) {
+    echo "ok";
 } else {
-    echo '<script>alert("Welcome to Geeks for Geeks")</script>';
+    echo "error";
 }
+
+echo " Message has been sent....!";
 ?>
